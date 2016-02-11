8df6787259393d7fbf89bef5f4990fcd591a9c13
Marco Pompili <marcs.pompili@gmail.com>
2016-02-11 20:56:13 +0100

    docker shortcuts

---
+++ docker.sh
@@ -0,0 +1,10 @@
+alias dk="docker"
+alias dkp="docker ps -a"
+alias dki="docker images"
+
+alias dc="docker-compose"
+alias dcup="docker-compose up"
+alias dcupd="docker-compose up -d"
+alias dcs="docker-compose stop"
+alias dcrm="docker-compose rm"
+alias dck="docker-compose stop && docker-compose rm"
