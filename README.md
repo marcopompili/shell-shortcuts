a8fea8385e234dd9a8dd4fabf9abd53f8f33ce7b
Marco Pompili <marcs.pompili@gmail.com>
2016-02-11 20:56:02 +0100

    common listing

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # shell-shortcuts
 
 My day to day shortcuts for cruising the shell as fast as possible.
+
+## Usage
+
+Just copy the shell files in the folder:
+
+```shell
+/etc/profile.d
+```
+
+File permissions shouldn't matter.
---
+++ listing.sh
@@ -0,0 +1,14 @@
+alias c="cd"
+alias c.="cd ."
+alias c..="cd .."
+alias cd.="cd ."
+alias cd..="cd .."
+alias cl="clear"
+
+alias l="ls"
+alias ll="ls -l"
+alias lh="ls -lh"
+alias la="ls -la"
+
+alias s="sudo"
+alias wd="sudo -u www-data"
